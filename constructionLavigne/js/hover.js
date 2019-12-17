3b06f59175db262a2be9f2335f4862e89f91070c
Skoomaa <lavigne.felix@carrefour.cegepvicto.ca>
2019-12-16 19:43:52 -0500

    corr3

---
+++ constructionLavigne/js/hover.js
@@ -0,0 +1,10 @@
+
+$( "#jq1" ).hover(function() {
+    $("#jq1").toggleClass("dark");
+});
+$( "#jq2" ).hover(function() {
+    $("#jq2").toggleClass("dark");
+});
+$( "#jq3" ).hover(function() {
+    $("#jq3").toggleClass("dark");
+});
